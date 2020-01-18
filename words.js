f07bf2b24305f0886c378fd2567a80b4ad39fa48
farhado65 <farhadojemmo@yahoo.com>
2020-01-18 22:23:56 +0200

    Initial commit

---
+++ words.js
@@ -0,0 +1,14 @@
+const words = [
+    "engine",
+    "Linked In",
+    "login",
+    "hello",
+    "Java Script",
+    "Amsterdam",
+    "123"
+];
+var text = words.sort()
+     .filter(word => word.charAt(0) >= "A" && word.charAt(0) <= "Z" )
+     .join("<br/>");  
+     
+document.getElementById("words").innerHTML = text;     
